7ccb70f944407d586444bc49a923e4896b36b84d
Wooseong Chung <jungyh0528@gmail.com>
2024-08-06 14:59:04 +0900

    fix: not use exit code

--- check-image.sh
+++ check-image.sh
@@ -14,11 +14,4 @@ echo "Check if $IMAGE:$TAG image already been deployed to the Dockerhub"
 
 HTTP_CODE=$(wget --server-response --spider "https://hub.docker.com/v2/repositories/$IMAGE/tags/$TAG?page_size=100" 2>&1 | grep "HTTP/" | awk '{print $2}' | head -n 1)
 echo "HTTP_CODE: $HTTP_CODE"
-
-if [ "$HTTP_CODE" -eq 200 ]; then
-    echo "Image already exists. Skipping build."
-    exit 0
-else
-    echo "Image does not exists. Starting build."
-    exit 1
-fi
+echo $HTTP_CODE > http_code.txt
